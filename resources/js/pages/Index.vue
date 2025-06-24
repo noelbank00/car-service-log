ac1e5e15144645052370d339367991f8c974dbf0
Noel Bánk <noel.bank@smartleap.hu>
2025-06-24 13:25:29 +0200

    Refactor table to flex and grid

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -17,34 +17,30 @@ const clients = computed(() => page.props.clients.data);
 <template>
     <Head title="Dashboard" />
     <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-        <div class="w-full scroll-auto">
-            <div class="relative overflow-x-auto">
-                <table class="w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400">
-                    <thead class="bg-gray-50 text-xs text-gray-700 uppercase dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th class="px-6 py-3">Ügyfél azonosító</th>
-                            <th class="px-6 py-3">Név</th>
-                            <th class="px-6 py-3">Okmányazonosító</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr
-                            class="border-b border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800"
-                            v-for="client in clients"
-                            :key="client.id"
-                        >
-                            <td class="px-6 py-4 font-medium whitespace-nowrap text-gray-900 dark:text-white">
-                                {{ client.id }}
-                            </td>
-                            <td class="px-6 py-4 font-medium whitespace-nowrap text-gray-900 dark:text-white">
-                                {{ client.name }}
-                            </td>
-                            <td class="px-6 py-4 font-medium whitespace-nowrap text-gray-900 dark:text-white">
-                                {{ client.card_number }}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+        <div class="w-full overflow-x-auto">
+            <div class="min-w-full">
+                <!-- Header -->
+                <div class="grid grid-cols-3 bg-gray-50 text-xs text-gray-700 uppercase dark:bg-gray-700 dark:text-gray-400">
+                    <div class="px-6 py-3">Ügyfél azonosító</div>
+                    <div class="px-6 py-3">Név</div>
+                    <div class="px-6 py-3">Okmányazonosító</div>
+                </div>
+                <!-- Rows -->
+                <div
+                    v-for="client in clients"
+                    :key="client.id"
+                    class="grid grid-cols-3 border-b border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                    <div class="px-6 py-4 font-medium whitespace-nowrap text-gray-900 dark:text-white">
+                        {{ client.id }}
+                    </div>
+                    <div class="px-6 py-4 font-medium whitespace-nowrap text-gray-900 dark:text-white">
+                        {{ client.name }}
+                    </div>
+                    <div class="px-6 py-4 font-medium whitespace-nowrap text-gray-900 dark:text-white">
+                        {{ client.card_number }}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
